05f30464cd437d9d43f3c5327ae1834c0802630c
Junsheng Guo <junsheng@impactradius.com>
2017-09-06 13:18:58 -0700

    test

---
+++ cocoapodsTest/include/cocoapodsTest/AnotherHeader.h
@@ -0,0 +1,13 @@
+//
+//  AnotherHeader.h
+//  cocoapodsTest
+//
+//  Created by Junsheng on 9/6/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+#ifndef AnotherHeader_h
+#define AnotherHeader_h
+
+
+#endif /* AnotherHeader_h */
---
+++ cocoapodsTest/include/cocoapodsTest/cocoapodsTest.h
@@ -0,0 +1,15 @@
+//
+//  cocoapodsTest.h
+//  cocoapodsTest
+//
+//  Created by Junsheng on 9/6/17.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cocoapodsTest : NSObject
+
+- (void) method1;
+- (void) method2;
+@end
